4857dfd1db668216d15862411218acc199364473
Fred Sheppard <frederickjssheppard@gmail.com>
2023-01-21 21:15:49 +0000

    Add function to copy windSpeed data from influx to SQL

--- src/Batch.java
+++ src/Batch.java
@@ -5,54 +5,126 @@ import org.influxdb.dto.QueryResult;
 import org.influxdb.dto.QueryResult.Result;
 import org.influxdb.dto.QueryResult.Series;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+/**
+ * Class to upload a large amount of data from InfluxDB to SQL
+ */
 public class Batch {
 
-    static int limitPerCall = 10;
+    static final String DATABASE = "Influx 1";
+    static final int LIMIT_PER_CALL = 10;
+    static final String ORIGINAL_QUERY = """
+            SELECT * FROM "autogen"."environment.wind.speedTrue" WHERE "source"='derived-data'""";
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException, RuntimeException, IOException {
+        // This is a heavy operation that also clears the existing data from SQL
+        // We need to ensure the user hasn't run this by error
+        System.out.println("This will clear the database. Proceed?");
+        Scanner input = new Scanner(System.in);
+        boolean valid = false;
+        while (!valid) {
+            System.out.println("Y/N?");
+            String s = input.nextLine().toLowerCase();
+            if (s.equals("y")) {
+                valid = true;
+            } else if (s.equals("n")) {
+                System.out.println("Cancelling");
+                System.exit(1);
+            }
+        }
+
+        PrintWriter logger = new PrintWriter(new BufferedWriter(new FileWriter("log.txt")));
         try (InfluxDB influx = InfluxDBFactory.connect("http://localhost:8086", "root", "0")) {
-            influx.setDatabase("Influx 1");
+            influx.setDatabase(DATABASE);
             OffsetDateTime previousFinalTime = OffsetDateTime.parse("1970-01-01T00:00:00.000Z");
-            String originalQuery = "select * from \"autogen\".\"environment.wind.speedTrue\"";
             @SuppressWarnings("SpellCheckingInspection")
             DateTimeFormatter myPattern = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.nnnnnnnnn'Z'");
+            Statement sqlStatement;
+            sqlStatement = DriverManager.getConnection(
+                            "jdbc:mariadb://localhost:3307/Boat",
+                            "root",
+                            "1234")
+                    .createStatement();
+            clearTable(sqlStatement);
+
             int counter = 0;
-            int iterator = 0;
-            while (iterator < 10) {
+            while (true) {
+                StringBuilder builder = new StringBuilder("INSERT INTO data (Epoch, WindSpeed) VALUES\n");
                 try {
-                    System.out.println();
                     String query = String.format(
-                            "%s where time > '%s' limit %d",
-                            originalQuery, previousFinalTime.format(myPattern), limitPerCall);
+                            "%s and time > '%s' limit %d",
+                            ORIGINAL_QUERY, previousFinalTime.format(myPattern), LIMIT_PER_CALL);
                     var values = getValues(influx.query(new Query(query)));
-                    for (List<Object> value : values) {
-                        // Todo add to SQL insert query
-                        System.out.println(value);
-                        counter++;
+                    if (values == null) {
+                        break;
                     }
+                    for (List<Object> value : values) {
+                        try {
+                            String timeString = (String) value.get(0);
+                            OffsetDateTime time = OffsetDateTime.parse(timeString);
+                            // Get every fifth value
+                            if (counter++ % 5 != 0) {
+                                continue;
+                            }
+                            double windSpeed = ((Double) value.get(4));
+                            builder.append("(");
+                            builder.append(time.toEpochSecond());
+                            builder.append(",");
+                            builder.append(windSpeed);
+                            builder.append("),\n");
+                        } catch (Exception e) {
+                            // Most commonly caused by null values in the database
+                            logger.println(counter + ": " + value);
+                        }
+                    }
+                    System.out.print("\r" + counter);
+                    int length = builder.length();
+                    builder.delete(length - 2, length);
+                    builder.append(";");
+                    sqlStatement.executeUpdate(builder.toString());
 
                     // Get the last time that was called
                     // This will be the starting point for the next batch
                     String timeString = (String) values.get(values.size() - 1).get(0);
                     previousFinalTime = OffsetDateTime.parse(timeString);
-                    iterator++;
-                } catch (Exception e) {
-                    System.out.println("Finished");
-                    break;
+                } catch (SQLException e) {
+                    System.out.println(builder);
+                    throw (e);
                 }
             }
-            System.out.println(counter);
+
+            logger.flush();
+            logger.close();
+            System.out.println("\nFinished");
         }
     }
 
+    /**
+     * Returns a list of values from an Influx result set
+     *
+     * @param results The result of an Influx query
+     * @return A new flat-packed list of values from the query
+     */
     private static ArrayList<List<Object>> getValues(QueryResult results) {
         ArrayList<List<Object>> appendList = new ArrayList<>();
         for (Result result : results.getResults()) {
+            List<Series> allSeries = result.getSeries();
+            if (allSeries == null) {
+                return null;
+            }
             for (Series series : result.getSeries()) {
                 appendList.addAll(series.getValues());
             }
@@ -60,4 +132,26 @@ public class Batch {
         return appendList;
     }
 
+    /**
+     * Clears the Boat/data SQL table by dropping it and recreating it.
+     *
+     * @param sqlStatement Statement used to execute the query
+     */
+    private static void clearTable(Statement sqlStatement) throws SQLException {
+        System.out.println("Dropping table");
+        sqlStatement.executeUpdate("DROP TABLE DATA");
+        System.out.println("Recreating table");
+        System.out.println("Querying...");
+        sqlStatement.executeUpdate("""
+                create table data
+                (
+                    ID        int auto_increment
+                        primary key,
+                    Epoch     bigint null,
+                    WindDir   int    null,
+                    WindGust  float  null,
+                    WindSpeed float  null
+                );
+                """);
+    }
 }
--- src/InfluxToSQL.java
+++ src/InfluxToSQL.java
@@ -15,26 +15,25 @@ public class InfluxToSQL {
 
     public static void main(String[] args) {
         String query = "select * from \"autogen\".\"environment.wind.speedApparent\"";
-        try (InfluxDB influx = InfluxDBFactory.connect("http://localhost:8086", "root", "0")) {
-            influx.setDatabase("Influx 1");
-            var values = getValues((influx.query(new Query(query))).getResults());
+        InfluxDB influx = InfluxDBFactory.connect("http://localhost:8086", "root", "0");
+        influx.setDatabase("Influx 1");
+        var values = getValues((influx.query(new Query(query))).getResults());
 
-            StringBuilder builder = new StringBuilder("INSERT INTO data (Epoch, WindSpeed) VALUES\n");
-            for (var value : values) {
-                String timeString = ((String) value.get(0));
-                long time = OffsetDateTime.parse(timeString).toEpochSecond();
-                double windSpeed = ((Double) value.get(3));
-                builder.append("(");
-                builder.append(time);
-                builder.append(",");
-                builder.append(windSpeed);
-                builder.append("),\n");
-            }
-            int length = builder.length();
-            builder.delete(length - 2, length);
-            builder.append(";");
-            System.out.println(builder);
+        StringBuilder builder = new StringBuilder("INSERT INTO data (Epoch, WindSpeed) VALUES\n");
+        for (var value : values) {
+            String timeString = ((String) value.get(0));
+            long time = OffsetDateTime.parse(timeString).toEpochSecond();
+            double windSpeed = ((Double) value.get(3));
+            builder.append("(");
+            builder.append(time);
+            builder.append(",");
+            builder.append(windSpeed);
+            builder.append("),\n");
         }
+        int length = builder.length();
+        builder.delete(length - 2, length);
+        builder.append(";");
+        System.out.println(builder);
     }
 
     private static ArrayList<List<Object>> getValues(List<Result> results) {
